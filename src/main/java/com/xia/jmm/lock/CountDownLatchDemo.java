59504d668a6c7ba335be99f3744e98581f6e9bfd
xiachuang <1979277742@qq.com>
2020-09-21 01:21:13 +0800

    3

---
+++ src/main/java/com/xia/jmm/lock/CountDownLatchDemo.java
@@ -0,0 +1,8 @@
+package com.xia.jmm.lock;
+
+public class CountDownLatchDemo {
+    private final int  Count=10;
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/xia/jmm/lock/ReadWriterLockDemo.java
+++ src/main/java/com/xia/jmm/lock/ReadWriterLockDemo.java
@@ -1,4 +1,44 @@
 package com.xia.jmm.lock;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class  myCache{
+    private  final int Age=10;
+//    private volatile AtomicReference<Thread> atomicReference=new AtomicReference<>();
+    private volatile Map<String,Object> map=new HashMap<>();
+    private ReentrantReadWriteLock rwLock=new ReentrantReadWriteLock();//读写锁
+
+    public void put(){
+        //设置 模块化编程  加快编程速度
+        rwLock.writeLock().lock(); //获取 写锁
+                try {
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    rwLock.writeLock().unlock();
+                }
+    }
+    public void get(){
+         rwLock.readLock().lock();//获得写锁
+                 try {
+//                   int age=Age;
+                 }catch (Exception e){
+                     e.printStackTrace();
+                 }finally {
+                     rwLock.readLock().unlock();
+                 }
+    }
+}
 public class ReadWriterLockDemo {
+    public static void main(String[] args) {
+      for(int i=1;i<=10;i++){
+                  new Thread(()->{
+
+                  },String.valueOf(i)).start();
+              }
+    }
 }
